63272fb47dbce6d74faf5b363b299a307c73f4fe
DmitriyChistyakov <dinacher@mail.ru>
2021-05-22 13:23:17 +0300

    Домашнее задание по Рефлексии и Аннотациям

---
+++ src/ReflectionAndAnnotation/AfterSuite.java
@@ -0,0 +1,13 @@
+package ReflectionAndAnnotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+
+public @interface AfterSuite {
+    String prioritet () default "10000";
+}
---
+++ src/ReflectionAndAnnotation/TestClass.java
@@ -0,0 +1,78 @@
+package ReflectionAndAnnotation;
+
+public class TestClass {
+    private int prioritet;
+
+    public TestClass(){
+
+    }
+
+    @Test (prioritet = "1")
+    public void doTest1 (){
+        prioritet = 1;
+        System.out.println("Начало теста №1:....... ");
+    }
+
+    @Test (prioritet = "2")
+    public void doTest2 (){
+        prioritet = 2;
+        System.out.println("Начало теста №2:......... ");
+
+    }
+
+
+    @Test(prioritet = "3")
+    public void doTest3 (){
+        prioritet = 3;
+        System.out.println("Начало теста №3:...... ");
+
+    }
+
+
+
+    @Test(prioritet = "4")
+    public void doTest4 (){
+        prioritet = 4;
+        System.out.println("Начало теста №4:...... ");
+
+    }
+
+
+
+    @Test(prioritet = "5")
+    public void doTest5 (){
+        prioritet = 5;
+        System.out.println("Начало теста №5:...... ");
+
+    }
+//
+//    @Test(name = "BeforeSuite")
+//    public void BeforeSuite (){
+//        prioritet = 5;
+//        System.out.println("Начало теста №5:...... ");
+//
+//    }
+//
+//    @Test(name = "AfterSuite", prioritet = 10000)
+//    public void AfterSuite (){
+//        prioritet = 10000;
+//        System.out.println("Начало теста №5:...... ");
+//
+//    }
+
+    @BeforeSuite (prioritet = "0")
+    public void beforeSuite (){
+        System.out.println("Начало первоначального теста до выполнения тестов....");
+    }
+
+
+
+    @AfterSuite(prioritet = "11111")
+    public void afterSuite (){
+        System.out.println("Начало конечного теста после выполнения ....");
+    }
+
+
+
+
+}
